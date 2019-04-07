47e3cd4b496c09c15c6071a9e24e6c25ef78acde
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2019-04-07 17:23:25 +0300

    remake project

--- Header/TTextLink.h
+++ Header/TTextLink.h
@@ -8,16 +8,17 @@ public:
 	char str[MaxLen];
 	TTextLink *pNext, //Следующее звено
 		*pDown;		  //Вложенное звено
-	TTextLink( char*_str = NULL, TTextLink* _pNext = NULL, TTextLink* _pDown = NULL);
-};
-TTextLink::TTextLink( char*_str, TTextLink* _pNext, TTextLink* _pDown) 
-{
- 
-	pNext = _pNext;
-	pDown = _pDown;
-	if (_str == NULL)
-		str[0] = '\0';
-	else
-		strcpy(str, _str);
+	TTextLink(char*_str = NULL, TTextLink* _pNext = NULL, TTextLink* _pDown = NULL) 
+	{
+		
 
-}
+			pNext = _pNext;
+			pDown = _pDown;
+			if (_str == NULL)
+				str[0] = '\0';
+			else
+				strcpy(str, _str);
+
+		
+	}
+};
